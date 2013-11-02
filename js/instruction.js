1b4e02fcb17481208aa9adc037ebae0edea33d91
Checc <ken123143@hotmail.com>
2013-11-02 22:56:23 +0800

    instruction and icon

---
+++ js/instruction.js
@@ -0,0 +1,31 @@
+function instruction()
+{
+	     
+         $(".mask").css("display","block");  
+		 $(".instruction1").show(1000);
+		  $(".instruction1").click(function(){
+		  	$(".instruction1").hide(1000);
+			$(".instruction2").show(1000);
+		  });
+		  
+		  $(".instruction2").click(function(){
+			
+			$(".instruction2").hide(1000);
+			$(".instruction3").show(1000);
+		  });
+		  
+		  $(".instruction3").click(function(){
+			$(".instruction3").hide(1000);
+			$(".mask").fadeOut(2000);
+		  });
+
+		
+		
+		/*$("body").mousedown(function(){
+			
+		
+			
+			$(".mask").hide();
+				});*/
+}
+	$(document).ready(instruction);
