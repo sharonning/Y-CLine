28bcc0b7a2ac85f5e86d7f5339128fd4e3d48918
twooo333 <twooo333chen@gmail.com>
2013-11-03 10:18:19 +0800

    final

--- timeline.php
+++ timeline.php
@@ -24,7 +24,7 @@
 	 
 	 function unsearch(){
 		$.timeliner({
-				startOpen:['#195405170000EX']
+				startOpen:['#19550828EX']
 			});
 			// Colorbox Modal
 			$(".CBmodal").colorbox({inline:true, initialWidth:100, maxWidth:682, initialHeight:100, transition:"elastic",speed:750});
@@ -94,179 +94,64 @@ for($i=1;$i<=5;$i++){
 			
 			<br class="clear">
 
-			<div class="timelineMajor">
-				<h2 class="timelineMajorMarker"><span>2013 December</span></h2>
-				<dl class="timelineMinor">
-					<dt id="195405170000"><a>Brown v. Board of Education</a></dt>
-					<dd class="timelineEvent" id="195405170000EX" style="display:none;">
-						<h3>May 17, 1954</h3>
-						<p>
-							The U.S. Supreme Court hands down a unanimous 9-0 decision in the Brown v. Board of Education of Topeka case, opening the door for the civil rights movement and ultimately racial integration in all aspects of U.S. society. In overturning Plessy v. Ferguson (1896), the court rules that “separate educational facilities are inherently unequal.”<sup>1</sup></p>
-
-							<br class="clear">
-					</dd><!-- /.timelineEvent -->
-				</dl><!-- /.timelineMinor -->
-			</div><!-- /.timelineMajor -->
+			
 
 			<div class="timelineMajor">
 				<h2 class="timelineMajorMarker"><span>2013 November</span></h2>
 				<dl class="timelineMinor">
-					<dt id="19550828"><a>Emmett Till</a></dt>
+					<dt id="19550828"><a>在yahoo...</a></dt>
 					<dd class="timelineEvent" id="19550828EX" style="display:none;">
-						<h3>August 28, 1955</h3>
+						<h3>November 3, 2013</h3>
 
 						<div class="media">
-							<a href="#inline-1955-08-282" class="CBmodal"><img src="http://img.youtube.com/vi/GU1wuqyOP98/0.jpg" width="240" height="180" alt="Related Video: The Emmett Till Murder"></a>
-							<p class="mediaLink"><a href="#inline-1955-08-282" class="CBmodal" title="Related Video: The Emmett Till Murder">Watch: The Emmett Till Murder</a></p>
+							<a href="#inline-1955-08-282" class="CBmodal"><img src="https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-prn2/v/1391686_632284440127771_54880886_n.jpg?oh=5ed88f22d591f83e557d6a86e443f5fa&oe=5277DA7C&__gda__=1383624254_30b788eb7ecbe728196bf782b12af636" width="240" height="180" alt="Related Video: The Emmett Till Murder"></a>
+							<p class="mediaLink"><a href="#inline-1955-08-282" class="CBmodal" title="Related Video: The Emmett Till Murder"></a></p>
 							<div style="display:none">
 								<div id="inline-1955-08-282" class="modalBox">
-									<object>
-										<param name="movie" value="http://www.youtube.com/v/GU1wuqyOP98?fs=1&amp;hd=0&amp;showsearch=0&amp;showinfo=0&amp;width=640&amp;height=380">
-										<param name="allowFullScreen" value="true">
-										<param name="allowScriptAccess" value="always">
-										<embed src="http://www.youtube.com/v/GU1wuqyOP98?fs=1&amp;hd=0&amp;showsearch=0&amp;showinfo=0&amp;width=640&amp;height=380" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="640" height="380">
-										</object>
+									<img src="https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-prn2/v/1391686_632284440127771_54880886_n.jpg?oh=5ed88f22d591f83e557d6a86e443f5fa&oe=5277DA7C&__gda__=1383624254_30b788eb7ecbe728196bf782b12af636">
 									</div>
 								</div>
 						</div><!-- /.media -->
 
 						<p id="195508280">
-							Fourteen-year-old African-American Emmett Till is brutally murdered after reportedly flirting with a white woman while visiting relatives in Mississippi. For the first time, both black and white reporters cover the trial epitomizing “one of the most shocking and enduring stories of the twentieth century.”<sup>2</sup> The white defendants, Roy Bryant and J.W. Milam, are acquitted by an all-white jury in only 67 minutes; later they describe in full detail to Look magazine (which paid them $4,000) how they killed Till.<sup>3</sup> His mother insists on an open casket funeral, and the powerful image of his mutilated body sparks a strong reaction across the country and the world.</p>
+							在yahoo day好好玩!<br>
+							第一天有很多課程還有樂團表演!<br>
+							看到好多跟我一樣是學生的隊伍!<br>
+							第一天晚上我們幾乎沒睡,de了好久的bug XD<br>
+						    第二天早上還有超HIGH早操活動!<br>
+							兩天有一半的時間都一直在吃!<br>
+						    好想睡ING!!<br>
+							但是真的值回票價啦!!<br>
+							Yahoo Rock!!<br></p>
 
 						<br class="clear">
 					</dd><!-- /.timelineEvent -->
 				</dl><!-- /.timelineMinor -->
 
 
-				<dl class="timelineMinor">
-					<dt id="195512010000"><a>Rosa Parks</a></dt>
-					<dd class="timelineEvent" id="195512010000EX" style="display:none;">
-						<h3>December 1, 1955</h3>
-						<p>
-							The arrest of Rosa Parks, a 42-year-old African-American seamstress and civil rights activist who refused to give up her bus seat to a white passenger, sets off a long anticipated bus boycott by residents of Montgomery, Ala. The 13-month protest and ensuing litigation eventually make it to the U.S. Supreme Court, which declares that segregation on public buses is unconstitutional.<sup>4</sup> The Montgomery bus boycott brings the Rev. Dr. Martin Luther King Jr. and his nonviolent approach to social change to the forefront of the civil rights movement.</p>
-
-							<br class="clear">
-					</dd><!-- /.timelineEvent -->
-				</dl><!-- /.timelineMinor -->
+				
 			</div><!-- /.timelineMajor -->
 
 			<div class="timelineMajor">
 				<h2 class="timelineMajorMarker"><span>2013 October</span></h2>
-				<dl class="timelineMinor">
-					<dt id="19570904"><a>Little Rock</a></dt>
-					<dd class="timelineEvent" id="19570904EX" style="display:none;">
-						<h3>September 4, 1957</h3>
-
-						<div class="media">
-							<a href="#inline-1957-09-044" class="CBmodal"><img src="http://img.youtube.com/vi/h148GEIgUeA/0.jpg" width="240" height="180" alt="Related Video: Reporting Little Rock"></a>
-							<p class="mediaLink"><a href="#inline-1957-09-044" class="CBmodal" title="Related Video: Reporting Little Rock">Watch: Reporting Little Rock</a></p>
-							<div style="display:none">
-								<div id="inline-1957-09-044" class="modalBox">
-									<object>
-										<param name="movie" value="http://www.youtube.com/v/h148GEIgUeA?fs=1&amp;hd=0&amp;showsearch=0&amp;showinfo=0&amp;width=640&amp;height=380">
-										<param name="allowFullScreen" value="true">
-										<param name="allowScriptAccess" value="always">
-										<embed src="http://www.youtube.com/v/h148GEIgUeA?fs=1&amp;hd=0&amp;showsearch=0&amp;showinfo=0&amp;width=640&amp;height=380" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="640" height="380">
-										</object>
-									</div>
-								</div>
-						</div><!-- /.media -->
-
-						<p>
-							Three years removed from the Brown v. Board of Education decision, Arkansas Gov. Orval Faubus orders the National Guard to stop nine black students from attending the all-white Little Rock Central High School. President Dwight D. Eisenhower intervenes by federalizing the National Guard and deploying Army troops to protect the students, stripping the state of power. Media coverage of the physical and verbal harassment the black students were subjected to is reported and broadcast around the world. In the end, they successfully integrate Central High. <sup>5</sup></p>
-
-						<br class="clear">
-					</dd><!-- /.timelineEvent -->
-				</dl><!-- /.timelineMinor -->
+				
 			</div><!-- /.timelineMajor -->
 
 			<div class="timelineMajor">
 				<h2 class="timelineMajorMarker"><span>2013 September</span></h2>
-				<dl class="timelineMinor">
-					<dt id="19610504"><a>Freedom Rides</a></dt>
-					<dd class="timelineEvent" id="19610504EX" style="display:none;">
-						<h3>May 4, 1961</h3>
-
-						<div class="media">
-							<a href="#inline-1961-05-045" class="CBmodal"><img src="http://img.youtube.com/vi/Sxe9dJoZ-AQ/0.jpg" width="240" height="180" alt="Related Video: Freedom Rides"></a>
-							<p class="mediaLink"><a href="#inline-1961-05-045" class="CBmodal" title="Related Video: Freedom Rides">Watch: Freedom Rides</a></p>
-							<div style="display:none">
-								<div id="inline-1961-05-045" class="modalBox">
-									<object>
-										<param name="movie" value="http://www.youtube.com/v/Sxe9dJoZ-AQ?fs=1&amp;hd=0&amp;showsearch=0&amp;showinfo=0&amp;width=640&amp;height=380">
-										<param name="allowFullScreen" value="true">
-										<param name="allowScriptAccess" value="always">
-										<embed src="http://www.youtube.com/v/Sxe9dJoZ-AQ?fs=1&amp;hd=0&amp;showsearch=0&amp;showinfo=0&amp;width=640&amp;height=380" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="640" height="380">
-										</object>
-									</div>
-								</div>
-						</div><!-- /.media -->
-
-					<p>
-						The first of many civil rights “Freedom Rides” leaves Washington, D.C., for New Orleans. The Freedom Riders want to test the validity of the Supreme Court’s decision to outlaw racial segregation in bus terminals and through interstate bus travel.<sup>6</sup> Angry white mobs – with the blessing of Alabama law enforcement – meet the convoy in Anniston and Birmingham, brutally beating the Freedom Riders and firebombing one of the buses.<sup>7</sup></p>
-						<br class="clear">
-					</dd><!-- /.timelineEvent -->
-				</dl><!-- /.timelineMinor -->
+				
 			</div><!-- /.timelineMajor -->
 
 			<div class="timelineMajor">
 				<h2 class="timelineMajorMarker"><span>2013 August</span></h2>
-				<dl class="timelineMinor">
-					<dt id="19630828"><a>"I Have a Dream"</a></dt>
-					<dd class="timelineEvent" id="19630828EX" style="display:none;">
-						<h3>August 28, 1963</h3>
-						<div class="media">
-							<a href="#inline-1963-08-287" class="CBmodal"><img src="http://img.youtube.com/vi/gvAQE66jwcg/0.jpg" width="240" height="180" alt="Related Video: Black Press"></a>
-							<p class="mediaLink"><a href="#inline-1963-08-287" class="CBmodal" title="Related Video: Black Press">Watch: Black Press</a></p>
-							<div style="display:none">
-								<div id="inline-1963-08-287" class="modalBox">
-									<object>
-										<param name="movie" value="http://www.youtube.com/v/gvAQE66jwcg?fs=1&amp;hd=0&amp;showsearch=0&amp;showinfo=0&amp;width=640&amp;height=380">
-										<param name="allowFullScreen" value="true">
-										<param name="allowScriptAccess" value="always">
-										<embed src="http://www.youtube.com/v/gvAQE66jwcg?fs=1&amp;hd=0&amp;showsearch=0&amp;showinfo=0&amp;width=640&amp;height=380" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="640" height="380">
-										</object>
-									</div>
-								</div>
-						</div><!-- /.media -->
-
-						<p>
-							In one of the largest gatherings in the nation’s capital and one of the first to be broadcast live on national television, at least 200,000 civil rights protesters stage a March on Washington concluding at the Lincoln Memorial. The march is dedicated to jobs and freedom and takes place 100 years after the&nbsp;Emancipation Proclamation. The highlight of the event is Martin Luther King Jr.’s historic “I Have a Dream” speech.</p>
-
-						<blockquote>
-							I have a dream that one day this nation will rise up and live out the true meaning of its creed: "We hold these truths to be self-evident: that all men are created equal."
-							<div class="attribution">&mdash;Martin Luthar King Jr.</div>
-						</blockquote>
-
-						<br class="clear">
-					</dd><!-- /.timelineEvent -->
-				</dl><!-- /.timelineMinor -->
+				
 
 			</div><!-- /.timelineMajor -->
 
 			<div class="timelineMajor">
 				<h2 class="timelineMajorMarker"><span>2013 July</span></h2>
 
-				<dl class="timelineMinor">
-					<dt id="19640702"><a>Civil Rights Act</a></dt>
-					<dd class="timelineEvent" id="19640702EX" style="display:none;">
-						<h3>July 2, 1964</h3>
-						<p id="196407020">
-							President Lyndon B. Johnson signs the Civil Rights Act of 1964, mandating equal opportunity employment, and complete desegregation of schools and other public facilities. It also outlaws unequal voter registration requirements.<sup>13</sup> Although it would take years for these changes to take effect in communities around the country, the law is a monumental victory for the civil rights movement.</p>
-
-							<br class="clear">
-					</dd><!-- /.timelineEvent -->
-				</dl><!-- /.timelineMinor -->
-
-				<dl class="timelineMinor">
-					<dt id="19641014"><a>Nobel Peace Prize</a></dt>
-					<dd class="timelineEvent" id="19641014EX" style="display:none;">
-						<h3>October 14, 1964</h3>
-						<p>Martin Luther King Jr. is awarded the Nobel Peace Prize; at 35, he is the youngest recipient.</p>
-						<br class="clear">
-					</dd><!-- /.timelineEvent -->
-				</dl><!-- /.timelineMinor -->
-
+				
 			</div><!-- /.timelineMajor -->
 			<br class="clear">
 		</div><!-- /#timelineContainer -->
@@ -639,13 +524,7 @@ for($i=1;$i<=5;$i++){
 		
 		
     </script> 
-    <input type="button" id="fbLoginBtn" value="Login"/> | 
-    <input type="button" id="fbLogoutBtn" value="LogOut"/>
-
-    <input type="button" id="meBtn" value="取得使用者貼文" />
-	<input type="button" id="fun" value="test" />
-	<span id="infotest">00000000000000000test</span>
-	
+   
 	
 	<?php
 error_reporting(0);
